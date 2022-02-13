9308226872d24139a7d0fd2a89d117fba3053eea
taylor.holmes <taylorh2343@gmail.com>
2022-02-12 19:30:56 -0600

    added db folder, and index files

---
+++ db/seeds.sql
@@ -0,0 +1,7 @@
+INSERT INTO department (id, name)
+VALUES
+(1, 'Sales'),
+(2, 'Marketing'),
+(3, 'Accounting '),
+(4, 'Development'),
+(5, 'Executives')
---
+++ index.js
@@ -0,0 +1,21 @@
+const mysql = require('mysql2');
+const inquirer = require('inquirer');
+
+require('console.table');
+require('dotenv').config();
+
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        
+        user: process.env.DB_USER,
+        password: process.env.DB_PASSWORD,
+        database: process.env.DB_NAME
+    },
+    console.log(`Connected to the ${process.env.DB_NAME} database.`)
+);
+
+function showOptions(
+    
+)
+
